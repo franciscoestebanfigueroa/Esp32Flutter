56ea5665080e260850d7139079f0910d16a4983b
pancho <pancho@gmail.com>
2023-09-10 01:44:19 -0300

    cambiando boton de estado, data es un map..no json

--- lib/provider/model.dart
+++ lib/provider/model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
@@ -11,58 +13,61 @@ class Model with ChangeNotifier {
   String _uri = 'https://server-ia2.onrender.com';
 
   Model() {
-    
+    socket = IO.io(
+        //"http://localhost:3000",
+        'http://192.168.0.12:443',
+        // _uri,
+        IO.OptionBuilder()
+            .setTransports(['websocket']) // for Flutter or Dart VM
+            // .setExtraHeaders({"data": "xxx"}) // optional
+            //.enableAutoConnect()
+            .build());
+    print("creando instancia de socket io ");
+    socket.close();
   }
   on() {
-    
-
     _puerto2 = !_puerto2;
     print("estado puerto $_puerto2");
     notifyListeners();
+    socket
+        .emit("mensajex", {"nombre": "puerto2", "estado": _puerto2.toString()});
   }
 
   conectarSocket() {
-    try {
-      socket = IO.io(
-          //"http://localhost:3000",
-          //'http://192.168.0.12:3000',
-          _uri,
-          IO.OptionBuilder()
-              .setTransports(['websocket']) // for Flutter or Dart VM
-             // .setExtraHeaders({"data": "xxx"}) // optional
-              //.enableAutoConnect()
-              .build());
+    socket.connect();
 
-     // socket.close();
-     socket.connect();
-      print("creando instancia de socket io ");
+    socket.onConnect((data) {
+      _estdo_server = true;
+      notifyListeners();
 
-    socket.onConnect((data)  {
-      print("por finnnnn");
-      socket.emit("mensajex",{"nombre": "pancho" , "estado" : true});
+      socket.on("mensajex", (data) {
+        print("evaluando data ${data}");
+        try {
+          String nombre = data['nombre'];
+          String estado = data['estado'];
+          print('Nombre: $nombre');
+          print('Estado: $estado');
+        } catch (e) {
+          print('Error al decodificar JSON: $e');
+        }
+
+        if (data['estado'] == "false") {
+          _puerto2 = false;
+          notifyListeners();
+        } else {
+          _puerto2 = true;
+          notifyListeners();
+        }
       });
-    socket.onConnectError((e) =>
-      print("conectado a server..$e ")
-    );
+      print("por finnnnn");
+    });
+    socket.onConnectError((e) => print("conectado a server..$e "));
 
     socket.onDisconnect((data) {
       _estdo_server = false;
       print("se apago el server");
       notifyListeners();
-    });  
-      
-    
-    
-    
-    
-    } catch (e) {
-      print(e);
-    }
-   
-   //    _estdo_server = true;
-   // notifyListeners();
-
-    
+    });
   }
 
   desconectar() {
